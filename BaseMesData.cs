f04be4257e730f62343b2bba400b12e80a4b6b21
AhnYeonghoo <dksdudgn00@naver.com>
2023-12-18 14:00:00 +0900

    d

---
+++ BaseMesData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public abstract class BaseMesData
+    {
+        public BaseMesData() { }
+    }
+}
