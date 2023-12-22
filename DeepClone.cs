6e246c8a4dec4b9af1a4f14e1cfe66f3e0ad51fc
AhnYeonghoo <dksdudgn00@naver.com>
2023-12-22 14:00:00 +0900

    d

---
+++ DeepClone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class DeepClone
+    {
+        public static double NextGaussian(this Random r, double mu = 0, double sigma = 1)
+        {
+            var u1 = r.NextDouble();
+            var u2 = r.NextDouble();
+
+            var rand_std_normal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                                Math.Sin(2.0 * Math.PI * u2);
+
+            var rand_normal = mu + sigma * rand_std_normal;
+
+            return rand_normal;
+        }
+    }
+}
