81fb631cd3e88ea8fcadc43589ee934c5e5f2688
AhnYeonghoo <dksdudgn00@naver.com>
2023-12-23 14:00:00 +0900

    d

--- DeepClone.cs
+++ DeepClone.cs
@@ -18,5 +18,17 @@ namespace ConsoleApp1
 
             return rand_normal;
         }
+
+        public static T DeepClone<T>(this T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
     }
 }
